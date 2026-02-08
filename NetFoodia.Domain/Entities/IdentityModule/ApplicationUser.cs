df3cd0bbd8b1cf223389b696ed4287f048b9335d
Ahmed-Mohamed-Samy <AhmedSamy1ami@gmail.com>
2026-02-08 21:26:10 +0200

    feat: Add Application User

---
+++ NetFoodia.Domain/Entities/IdentityModule/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFoodia.Domain.Entities.IdentityModule
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = default!;
+        public UserRole Role { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
