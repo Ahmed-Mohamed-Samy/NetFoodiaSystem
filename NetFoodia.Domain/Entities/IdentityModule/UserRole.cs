353ae4176440060ce5d1956f06f76d0d6a7e52ff
Ahmed-Mohamed-Samy <AhmedSamy1ami@gmail.com>
2026-02-08 21:26:10 +0200

    feat: Add Application User

---
+++ NetFoodia.Domain/Entities/IdentityModule/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFoodia.Domain.Entities.IdentityModule
+{
+    public enum UserRole
+    {
+        Donor = 1,
+        Volunteer = 2,
+        CharityAdmin = 3,
+    }
+}
